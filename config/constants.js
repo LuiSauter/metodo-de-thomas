6aa8828d0a683c7dfe428d30619f93f5b8948268
Luis Gabriel Janco <janco7249@gmail.com>
2023-11-30 21:41:10 -0400

    feat: add constants colors and storage key

---
+++ config/constants.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+  // text color
+  text: '#25282b',
+  textSecondary: '#979797',
+  // base colors
+  primary: '#d3f26a',
+  secondary: '#ffffff',
+  tertiary: '#f6f6ef'
+}
+
+export default {
+  STORAGE_STATE: '@init-app'
+}
