b9214e76d6e13751baa0bcfd74eeb39c8a5ec1dd
Luis Gabriel Janco <janco7249@gmail.com>
2023-11-30 21:39:36 -0400

    feat: add slides data to Onboarding screen

---
+++ assets/data/slides.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: '1',
+    image: require('../images/welcome-1.png'),
+    title: 'Bienvenido al Método de Thomas',
+    description: 'El eficiente método para resolver sistema de ecuaciones tridiagonales.'
+  },
+  {
+    id: '2',
+    image: require('../images/metodo-thomas.png'),
+    title: 'Sistemas de ecuac. lineales tridiagonales',
+    description: 'La mayoría de los coeficientes son cero, excepto en la diagonal principal y dos diagonales adyacentes.'
+  },
+  {
+    id: '3',
+    image: require('../images/team.png'),
+    title: 'Integrantes',
+    description: 'Luis Gabriel Janco Alvarez\nMaría Ilse Romero Saavedra\nJaime Junior Roca Casanova\nJuan Daniel Ramos Prada\nJairo Vargas Figueroa'
+  }
+]
