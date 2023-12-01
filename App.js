ca08df42658cca95bfdcb75ce029739e0577d827
Luis Gabriel Janco <janco7249@gmail.com>
2023-11-30 21:42:44 -0400

    feat: add root application

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
+import { StyleSheet } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import StackNavigation from './navigation'
+import { INITIAL_STATE, StateProvider } from './context/StateContext'
+import constants from './config/constants'
+import { useAsyncStorage } from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [state, setState] = useState(null)
+  const { getItem, setItem } = useAsyncStorage(constants.STORAGE_STATE)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const readItemFromStorage = async () => {
+    const item = await getItem()
+    setState(item || INITIAL_STATE)
+  }
+  useEffect(() => {
+    readItemFromStorage()
+  }, [])
+
+  return (
+    <StateProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StackNavigation state={state} />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </StateProvider>
+  )
+}
